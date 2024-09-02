053afea06d3f99dc8fb11ebec24757288dd220c1
azahar18 <mohammedazahar.shaik@zensar.com>
2024-09-02 15:06:04 +0530

    optimising file structure

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "create" {
 variable "name" {
   description = "Name to be used on EC2 instance created"
   type        = string
-  default     = ""
+  default     = "AZ-EC2-V1"
 }
 
 variable "ami_ssm_parameter" {
